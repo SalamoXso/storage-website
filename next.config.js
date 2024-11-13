9a0172644f97f99909127c84fd01c3ea2d127e33
Salamo <subfornothing1994@gamil.com>
2024-11-13 18:38:24 +0100

    fix to make _next/ repo accessible

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   output: 'export',
   assetPrefix: process.env.ASSET_PREFIX,
   basePath: process.env.BASE_PATH,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
 };
 
 module.exports = nextConfig;
