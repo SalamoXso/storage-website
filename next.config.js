df0d5c1c865426ff4581394af2dc170d2e87976b
Salamo <subfornothing1994@gamil.com>
2024-11-13 16:16:19 +0100

    fixed the styling css issue (nex/local/fonts)..

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: '/storage-website',  // Keeps paths prefixed with your desired base path
-  assetPrefix: './',  // Corrects the asset prefix to be absolute within your base path
+  assetPrefix: '/storage-website/',  // Corrects the asset prefix to be absolute within your base path
   output: 'export',             // Export for static build
 };
 
